47d56ce51a125f6f166c6d361e98ccec8c6fdd1e
Guilherme Podolak <podolak12345@gmail.com>
2022-06-29 20:08:04 -0300

    atualização requisito 1

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,10 +6,51 @@ class Form extends React.Component {
       <div>
         <form>
           <label htmlFor="name-input">
-            Nome do input:
             <input
               type="text"
               data-testid="name-input"
+            />
+          </label>
+
+          <label htmlFor="description-input">
+            <input
+              type="textArea"
+              data-testid="description-input"
+            />
+          </label>
+
+          <label htmlFor="attr1-input">
+            <input
+              type="number"
+              data-testid="attr1-input"
+            />
+          </label>
+
+          <label htmlFor="attr2-input">
+            <input
+              type="number"
+              data-testid="attr2-input"
+            />
+          </label>
+
+          <label htmlFor="attr3-input">
+            <input
+              type="number"
+              data-testid="attr3-input"
+            />
+          </label>
+
+          <label htmlFor="image-input">
+            <input
+              type="text"
+              data-testid="image-input"
+            />
+          </label>
+
+          <label htmlFor="save-button">
+            <input
+              type="button"
+              data-testid="save-button"
             />
           </label>
         </form>
